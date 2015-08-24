efa0ac049e1f97bfbb850255dcc88511a89ae56f
David Peek <david.peek@me.com>
2015-08-24 11:00:56 +0100

    Typed ReactComponent

--- src/lib/api/react/ReactComponent.hx
+++ src/lib/api/react/ReactComponent.hx
@@ -2,17 +2,21 @@ package api.react;
 
 import js.html.Element;
 
+typedef ReactComponent = TypedReactComponent<Dynamic, Dynamic>;
+
 @:native('React.Component')
-@:keep extern class ReactComponent
+@:keep extern class TypedReactComponent<TProps:Dynamic, TState:Dynamic>
 {
+	var props(default, null):TProps;
+	var state(default, null):TState;
 	var refs(default, null):Dynamic<ReactComponent>;
 
 	function getDOMNode():Element;
-	function setProps(nextProps:Dynamic):Void;
-	function replaceProps(nextProps:Dynamic):Void;
+	function setProps(nextProps:TProps):Void;
+	function replaceProps(nextProps:TProps):Void;
 	function transferPropsTo(targetComponent:ReactComponent):Void;
-	function setState(nextState:Dynamic, ?callback:Void -> Void):Void;
-	function replaceState(nextState:Dynamic, ?callback:Void -> Void):Void;
+	function setState(nextState:TState, ?callback:Void -> Void):Void;
+	function replaceState(nextState:TState, ?callback:Void -> Void):Void;
 	function forceUpdate(?callback:Void -> Void):Void;
 	function isMounted():Bool;
 
