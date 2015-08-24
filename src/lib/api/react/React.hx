01fdcb8dc423e41359d998c4822d8869db815a32
David Peek <david.peek@me.com>
2015-08-24 11:52:03 +0100

    Type ReactComponent.refs, add typedefs for common typed use cases

--- src/lib/api/react/React.hx
+++ src/lib/api/react/React.hx
@@ -4,15 +4,19 @@ import js.html.Element;
 import js.html.Event;
 import js.html.EventTarget;
 
+import api.react.ReactComponent;
+
 @:native('React')
 extern class React
 {
 	static var PropTypes(default, null):ReactPropTypes;
-	static function createElement(type:Dynamic, ?attrs:Dynamic, ?child1:Dynamic, ?child2:Dynamic, ?child3:Dynamic, ?child4:Dynamic, ?child5:Dynamic, ?child6:Dynamic, ?child7:Dynamic, ?child8:Dynamic, ?child9:Dynamic, ?child10:Dynamic):ReactComponent;
+	static function createElement(type:Dynamic, ?attrs:Dynamic,
+		?child1:Dynamic, ?child2:Dynamic, ?child3:Dynamic, ?child4:Dynamic, ?child5:Dynamic, 
+		?child6:Dynamic, ?child7:Dynamic, ?child8:Dynamic, ?child9:Dynamic, ?child10:Dynamic):ReactComponentOfDynamic;
 	static function initializeTouchEvents(flag:Bool):Void;
-	static function render(component:ReactComponent, container:Element, ?callback:Void -> Void):ReactComponent;
+	static function render(component:ReactComponentOfDynamic, container:Element, ?callback:Void -> Void):ReactComponentOfDynamic;
 	static function unmountComponentAtNode(container:Element):Bool;
-	static function renderComponentToString(component:ReactComponent):String;
-	static function renderComponentToStaticMarkup(component:ReactComponent):String;
-	static function findDOMNode(component:ReactComponent):Element;
+	static function renderComponentToString(component:ReactComponentOfDynamic):String;
+	static function renderComponentToStaticMarkup(component:ReactComponentOfDynamic):String;
+	static function findDOMNode(component:ReactComponentOfDynamic):Element;
 }
--- src/lib/api/react/ReactComponent.hx
+++ src/lib/api/react/ReactComponent.hx
@@ -2,27 +2,34 @@ package api.react;
 
 import js.html.Element;
 
-typedef ReactComponent = TypedReactComponent<Dynamic, Dynamic>;
-
 @:native('React.Component')
-@:keep extern class TypedReactComponent<TProps:Dynamic, TState:Dynamic>
+@:keep extern class ReactComponent<TProps:Dynamic, TState:Dynamic, TRefs:Dynamic>
 {
 	var props(default, null):TProps;
 	var state(default, null):TState;
-	var refs(default, null):Dynamic<ReactComponent>;
+	var refs(default, null):TRefs;
 
 	function getDOMNode():Element;
 	function setProps(nextProps:TProps):Void;
 	function replaceProps(nextProps:TProps):Void;
-	function transferPropsTo(targetComponent:ReactComponent):Void;
+	function transferPropsTo(targetComponent:ReactComponentOfDynamic):Void;
 	function setState(nextState:TState, ?callback:Void -> Void):Void;
 	function replaceState(nextState:TState, ?callback:Void -> Void):Void;
 	function forceUpdate(?callback:Void -> Void):Void;
 	function isMounted():Bool;
 
-	function render():ReactComponent;
+	function render():ReactComponentOfDynamic;
 	function componentWillMount():Void;
 	function componentDidMount():Void;
 	function componentWillUnmount():Void;
 	function componentDidUnmount():Void;
 }
+
+typedef ReactComponentRefs = Dynamic<ReactComponentOfDynamic>;
+typedef ReactComponentOfDynamic = ReactComponent<Dynamic, Dynamic, ReactComponentRefs>;
+typedef ReactComponentOfProps<TProps> = ReactComponent<TProps, Dynamic, ReactComponentRefs>;
+typedef ReactComponentOfState<TState> = ReactComponent<Dynamic, TState, ReactComponentRefs>;
+typedef ReactComponentOfRefs<TRefs> = ReactComponent<Dynamic, Dynamic, TRefs>;
+typedef ReactComponentOfPropsAndState<TProps, TState> = ReactComponent<TProps, TState, ReactComponentRefs>;
+typedef ReactComponentOfPropsAndRefs<TProps, TRefs> = ReactComponent<TProps, Dynamic, TRefs>;
+typedef ReactComponentOfStateAndRefs<TState, TRefs> = ReactComponent<TState, Dynamic, TRefs>;
