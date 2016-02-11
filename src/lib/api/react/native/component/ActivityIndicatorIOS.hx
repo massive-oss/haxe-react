cf5ef61cc271e8cc3f8f29a5751e80b9c29a2f57
kevinresol <kevinresol@gmail.com>
2016-02-11 18:22:52 +0800

    More stuffs

---
+++ src/lib/api/react/native/component/ActivityIndicatorIOS.hx
@@ -0,0 +1,8 @@
+package api.react.native.component;
+
+#if react_native
+
+@:jsRequire('react-native', 'ActivityIndicatorIOS')
+extern class ActivityIndicatorIOS{}
+
+#end
--- src/lib/api/react/native/component/Navigator.hx
+++ src/lib/api/react/native/component/Navigator.hx
@@ -5,7 +5,11 @@ package api.react.native.component;
 @:jsRequire('react-native', 'Navigator')
 extern class Navigator<T>
 {
+	public function getCurrentRoutes():Array<T>;
 	public function push(route:T):Void;
+	public function pop():Void;
+	public function replace(route:T):Void;
+	public function resetTo(route:T):Void;
 }
 
 @:jsRequire('react-native', 'Navigator.SceneConfigs')
