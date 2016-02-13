8200d99ca0057b4fabd972e8bfec14a7f6a9e01d
kevinresol <kevinresol@gmail.com>
2016-02-13 14:57:48 +0800

    Update alert

--- src/lib/api/react/native/api/Alert.hx
+++ src/lib/api/react/native/api/Alert.hx
@@ -5,7 +5,7 @@ package api.react.native.api;
 @:jsRequire('react-native', 'Alert')
 extern class Alert
 {
-	public static function alert(title:String, message:String, buttons:Array<Dynamic>):Void;
+	public static function alert(title:String, ?message:String, ?buttons:Array<{text:String, ?onPress:Void->Void}>, ?type:Dynamic):Void;
 }
 
 #end
