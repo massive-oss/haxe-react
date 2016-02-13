2ac19aca4d5d84c70145e740f666a9281e0eac87
kevinresol <kevinresol@gmail.com>
2016-02-13 18:30:24 +0800

    Added AsyncStorage

---
+++ src/lib/api/react/native/api/AsyncStorage.hx
@@ -0,0 +1,15 @@
+package api.react.native.api;
+
+#if react_native
+
+import js.Error;
+
+@:jsRequire('react-native', 'AsyncStorage')
+extern class AsyncStorage
+{
+	public static function getItem(key:String, ?callback:Error->String->Void):Void;
+	public static function setItem(key:String, value:String, ?callback:Error->Void):Void;
+	public static function removeItem(key:String, ?callback:Error->Void):Void;
+}
+
+#end
