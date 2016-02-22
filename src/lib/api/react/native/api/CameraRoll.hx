53fdcf1eda3b9fec489d182479bcfdd9a948dc61
kevinresol <kevinresol@gmail.com>
2016-02-22 17:38:39 +0800

    Type CameraRoll results properly

--- src/lib/api/react/native/api/CameraRoll.hx
+++ src/lib/api/react/native/api/CameraRoll.hx
@@ -6,7 +6,36 @@ import js.Promise;
 @:jsRequire('react-native', 'CameraRoll')
 extern class CameraRoll
 {
-	public static function getPhotos(params:Dynamic):Promise<Dynamic>;
+	public static function getPhotos(params:Dynamic):Promise<CameraRollResult>;
+}
+
+typedef CameraRollResult =
+{
+	edges:Array<{
+		node: {
+			timestamp:Float,
+			group_name:String,
+			type:String,
+			image: {
+				isStored:Bool,
+				uri:String,
+				height:Int,
+				width:Int,
+			},
+			location: {
+				speed:Float,
+				latitude:Float,
+				longitude:Float,
+				heading:Float,
+				altitude:Float,
+			}
+		}
+	}>,
+	page_info: {
+		has_next_page:Bool,
+		start_cursor:String,
+		end_cursor:String,
+	}
 }
 
 #end
