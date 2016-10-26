d3a53dd5b3ad8d7dea6c74e7e2552a15a83b0d2d
kevinresol <kevinresol@gmail.com>
2016-10-26 22:15:07 +0800

    Complete the API of NavigationStateUtils

--- src/lib/react/native/component/NavigationExperimental.hx
+++ src/lib/react/native/component/NavigationExperimental.hx
@@ -6,10 +6,22 @@ package react.native.component;
 extern class NavigationExperimental {}
 @:jsRequire('react-native', 'NavigationExperimental.CardStack')
 extern class CardStack {}
+
+// https://github.com/facebook/react-native/blob/3a8c302/Libraries/NavigationExperimental/NavigationStateUtils.js
 @:jsRequire('react-native', 'NavigationExperimental.StateUtils')
 extern class StateUtils {
-	public static function push(current:NavigationState, next:NavigationRoute):NavigationState;
-	public static function pop(current:NavigationState):NavigationState;
+	static function get(state:NavigationState, key:String):NavigationRoute;
+	static function indexOf(state:NavigationState, key:String):Int;
+	static function has(state:NavigationState, key:String):Bool;
+	static function push(state:NavigationState, route:NavigationRoute):NavigationState;
+	static function pop(state:NavigationState):NavigationState;
+	static function jumpToIndex(state:NavigationState, index:Int):NavigationState;
+	static function jumpTo(state:NavigationState, key:String):NavigationState;
+	static function back(state:NavigationState):NavigationState;
+	static function forward(state:NavigationState):NavigationState;
+	static function replaceAt(state:NavigationState, key:String, route:NavigationRoute):NavigationState;
+	static function replaceAtIndex(state:NavigationState, index:Int, route:NavigationRoute):NavigationState;
+	static function reset(state:NavigationState, routes:Array<NavigationRoute>, index:Int):NavigationState;
 }
 
 
