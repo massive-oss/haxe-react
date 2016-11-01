fe95ca5ac4694b1160e4b8b29f54fcfa87c406ed
terence <terence@awesapp.com>
2016-11-01 09:26:07 +0800

    make error callback to be optional input

--- src/lib/react/native/component/Image.hx
+++ src/lib/react/native/component/Image.hx
@@ -4,7 +4,7 @@ package react.native.component;
 
 @:jsRequire('react-native', 'Image')
 extern class Image{
-	public static function getSize(uri:String, success:Int->Int->Void , failure:Dynamic->Void):Void;
+	public static function getSize(uri:String, success:Int->Int->Void , ?failure:Dynamic->Void):Void;
 }
 
 #end
