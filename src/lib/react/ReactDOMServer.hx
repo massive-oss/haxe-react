56c1308c1f1d910770a73672df8811eee61330b0
Andy Li <andy@onthewings.net>
2019-09-11 06:07:10 +0800

    ReactDOMServer fixes
     * methods take ReactElement, not ReactComponent
     * add missing renderToNodeStream and renderToStaticNodeStream

--- src/lib/react/ReactDOMServer.hx
+++ src/lib/react/ReactDOMServer.hx
@@ -1,5 +1,7 @@
 package react;
 
+import react.ReactComponent;
+
 /**
 	https://facebook.github.io/react/docs/react-dom-server.html
 **/
@@ -12,10 +14,22 @@ extern class ReactDOMServer
 	/**
 		https://facebook.github.io/react/docs/react-dom-server.html#rendertostring
 	**/
-	public static function renderToString(component:ReactComponent):String;
+	public static function renderToString(component:ReactElement):String;
 
 	/**
 		https://facebook.github.io/react/docs/react-dom-server.html#rendertostaticmarkup
 	**/
-	public static function renderToStaticMarkup(component:ReactComponent):String;
+	public static function renderToStaticMarkup(component:ReactElement):String;
+
+	#if nodejs
+	/**
+		https://reactjs.org/docs/react-dom-server.html#rendertonodestream
+	**/
+	public static function renderToNodeStream(component:ReactElement):js.node.stream.Readable<Dynamic>;
+
+	/**
+		https://reactjs.org/docs/react-dom-server.html#rendertostaticnodestream
+	**/
+	public static function renderToStaticNodeStream(component:ReactElement):js.node.stream.Readable<Dynamic>;
+	#end
 }
